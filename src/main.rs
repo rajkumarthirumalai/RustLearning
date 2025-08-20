2dba55ff9f0602c5f322b292b19649f71dc2c444
rajkumar <rajkumarthirumalaikani@gmail.com>
2025-08-20 15:30:48 +0530

    dummy

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    // println!() it is a macro that prints the string to the console
+    //macro is used to output the string to the console
+    //macros are similar to functions but they do not follow same rules as functions
+
+    //rust compiler ignore spaces unlike python
+    //the entire code can be written fnmain(){println!("Hello, world!");}
+
+    
+    println!("Hello, world!");
+}
